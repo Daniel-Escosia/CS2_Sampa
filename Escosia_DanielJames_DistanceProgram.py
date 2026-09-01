ee4e1f3eb762b600fdb95d7f1df70da1caeb14d1
Daniel-Escosia <descosia2031@wvc.pshs.edu.ph>
2026-09-01 09:15:46 +0800

    Comments addition
    Added comments to clarify input processing and distance calculation.

--- Escosia_DanielJames_DistanceProgram.py
+++ Escosia_DanielJames_DistanceProgram.py
@@ -4,13 +4,19 @@ import math
 print("DISTANCE FINDER V.1")
 time.sleep(0.5)
 print("Put the format of your coordinate as X,Y, no spaces.")
+# Input.
 a = input("Coordinate 1 = ")
 b = input("Coordinate 2 = ")
+
+# Splitting the input, 0 for the first value (X) and 1 for the second (Y)
+# float turns the string to a float, meaning it can be subtracted and added to each other.
 x1 = float(a.split(",")[0])
 x2 = float(b.split(",")[0])
 y1 = float(a.split(",")[1])
 y2 = float(b.split(",")[1])
-   
+
+# Pythagorean Theorem and output.
+# sqrt sqaureroots the entire thing, while pow provides the power of 2 in the equation.
 dist = math.sqrt(math.pow(x2-x1, 2)+math.pow(y2-y1, 2))
 print("The distance is ", dist)
 
